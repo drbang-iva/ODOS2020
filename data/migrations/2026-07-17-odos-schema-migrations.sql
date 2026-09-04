8c9d6a075187920de22fa60c7b651dba77632294
DrBang <drbang@ivaeyecare.com>
2026-09-04 14:43:24 -0400

    Serialize migration ledger bootstrap

--- data/migrations/2026-07-17-odos-schema-migrations.sql
+++ data/migrations/2026-07-17-odos-schema-migrations.sql
@@ -1,4 +1,10 @@
+BEGIN;
+
+SELECT pg_advisory_xact_lock(hashtext('odos_schema_migrations'));
+
 CREATE TABLE IF NOT EXISTS odos_schema_migrations (
     filename TEXT PRIMARY KEY,
     applied_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
+
+COMMIT;
